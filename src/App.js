1141c61856d65cc84e1756c1f2f21d636a8a8397
Simon Rowland <simon@futrli.com>
2020-07-08 23:09:57 +0100

    Initial implementation of countdown to Wales

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Countdown from './Countdown.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const movingDateString = "2024-07-01T12:00:00";
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Leia and Si Move to Wales!</h2>  
+        </div>
+        <h3 className="title">Moving to Wales in ...</h3>
+        <Countdown date={movingDateString} />
+      </div>
+    )
+  }
 }
 
 export default App;
